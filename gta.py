dad1325ecf3f93aacd4570c1f470854548129a86
Mateusz Flis <flis15571@gmail.com>
2024-11-11 17:44:28 +0100

    3

--- gta.py
+++ gta.py
@@ -1,1 +1,14 @@
-print('CAT')
+# punkty = 42
+
+# if punkty >= 90:
+#     ocena = '5'
+# elif punkty >= 75:    
+#     ocena = '4'
+# elif punkty >= 60:
+#     ocena = '3'
+# else:
+#     ocena = '2'
+
+# # print(ocena)
+
+len('Datatypes')
